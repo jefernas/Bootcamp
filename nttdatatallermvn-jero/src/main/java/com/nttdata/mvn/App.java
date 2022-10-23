852cfe649c66f5cb96b92258990a3f6e9c5ac782
Jero <jefernas@emeal.nttdata.com>
2022-10-23 18:25:54 +0200

    añadido generacion de documentacion JavaDoc en proyecto nttdatatallermvn-jero

--- nttdatatallermvn-jero/src/main/java/com/nttdata/mvn/App.java
+++ nttdatatallermvn-jero/src/main/java/com/nttdata/mvn/App.java
@@ -6,23 +6,24 @@ import org.apache.commons.lang3.StringUtils;
 
 
 /**
- * Prueba de inyección de dependencias con maven
+ * Prueba de inyección de dependencias con maven + JavaDoc
  *
  */
 public class App 
 {
+	/** metodo main */
     public static void main( String[] args )
     {
-    	
+    	/** parámetros */
     	String mensaje = "Introduce un texto. Si introduces un texto tendrás 10 años de buena suerte. No dejes el texto sin introducir u ocurrirá una desgracia de nefastas consecuencias";
     	String msjVacio = "No has introducido nada, te has portado mal. Acabamos de informar telepáticamente a Papá Noel y los Reyes Magos";
 		String msjLleno = "¡¡Aprovecha tus 10 años de buena suerte!!";
     	String datos = JOptionPane.showInputDialog(null, mensaje);
 		
-		/*Uso de la clase StringUtils de la librería commons lang importada con maven
-    	*Detecta con la función .isEmpty de la clase StringUtils si el texto (no) introducido está en blanco, es nulo o ha introducido un espacio
+		/** Uso de la clase StringUtils de la librería commons lang importada con maven */
+    	/*Detecta con la función .isNoneBlank de la clase StringUtils si el texto (no) introducido está en blanco, es nulo o ha introducido un espacio
     	*y muestra un mensaje en consecuencia*/
-		if(StringUtils.isEmpty(datos)) {
+		if(!StringUtils.isNoneBlank(datos)) {
 			JOptionPane.showMessageDialog(null, msjVacio);
 		}else JOptionPane.showMessageDialog(null, msjLleno);
     }
